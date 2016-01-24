33cb78bfddb7644c276256545ffca39f92c2a0fe
Johnnylkc <momozzz1231@gmail.com>
2016-01-24 10:44:24 +0800

    Initial Commit

---
+++ ObjcScrollView0124/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjcScrollView0124
+//
+//  Created by 劉坤昶 on 2016/1/24.
+//  Copyright © 2016年 劉坤昶 Johnny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ObjcScrollView0124/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjcScrollView0124
+//
+//  Created by 劉坤昶 on 2016/1/24.
+//  Copyright © 2016年 劉坤昶 Johnny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
